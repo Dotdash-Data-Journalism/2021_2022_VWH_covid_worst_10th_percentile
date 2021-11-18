98748dddefab981bb29a77c57002dab32f5cf6d1
anesta95 <adriannesta@gmail.com>
2021-11-18 14:30:40 -0500

    Adding dash publish to api endpoint

--- covid19VaxUninsurance.R
+++ covid19VaxUninsurance.R
@@ -269,7 +269,7 @@ if (httr::status_code(notes_res) %in% c(200, 201, 202, 204)) {
 }
 
 publishRes <- POST(
-  url = "https://api.datawrapper.de/v3/charts/I3Kke",
+  url = "https://api.datawrapper.de/v3/charts/I3Kke/publish",
   add_headers(authorization = paste("Bearer", 
                                     DW_API, 
                                     sep = " "))
